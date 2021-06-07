2c5dff933486b400822bc483c40281ee1523316b
sergey <sergey.gojshik.81@gmail.com>
2021-06-07 18:46:02 +0200

    some

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
-
 $(document).ready(function(){
     $('.hamburger').click(function() {
         $('.hamburger').toggleClass("is-active"); 
         $('.header').toggleClass("display"); 
     });
+    $('.carousel__inner').slick({
+      
+        adaptiveHeight: true,
+        speed: 500,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left1.png" alt="left1"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt= "right"></button>',
+        responsive: [ 
+            {
+                breakpoint: 992,
+                settings: {
+                  arrows: false
+                }
+            }
+        ]
+    });
 });
+
